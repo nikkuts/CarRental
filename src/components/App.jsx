b608e0353eeb4b3855085f060de008f7670af3ca
nikkuts <nickkuts888@gmail.com>
2023-09-18 01:25:51 +0300

    updated App.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,9 @@ import { Route, Routes } from 'react-router-dom';
 
 import {SharedLayout} from './SharedLayout/SharedLayout';
 import {Home} from '../pages/Home';
-import {Catalog} from '../pages/Catalog';
+// import {Catalog} from '../pages/Catalog';
 import {Favorites} from '../pages/Favorites';
+import { AdvertsGallery } from "../components/AdvertsGallery/AdvertsGallery";
 
 export const App = () => {
     return (
@@ -12,7 +13,8 @@ export const App = () => {
           <Routes>
             <Route path="/" element={<SharedLayout />}>
                 <Route index element={<Home />} />
-                <Route path="/catalog" element={<Catalog />} />
+                {/* <Route path="/catalog" element={<Catalog />} /> */}
+                <Route path="/catalog" element={<AdvertsGallery/>} />
                 <Route path="/favorites" element={<Favorites />} />
                 <Route path="*" element={<Home />} />
             </Route>
