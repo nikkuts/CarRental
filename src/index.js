f5d1c134ffd191ebcb1062a451d9596c738b38ba
nikkuts <nickkuts888@gmail.com>
2023-09-18 09:29:58 +0300

    updated basename URL

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {App} from './components/App';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/CarRental">
+    <BrowserRouter basename="/car-rental">
       <App />
     </BrowserRouter>
   </React.StrictMode>
